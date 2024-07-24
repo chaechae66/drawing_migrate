57094a1f9bdf34bdbbfbd01812b8122cbd9c0623
chaechae66 <valueworkerchae@gmail.com>
2024-07-24 20:59:20 +0900

    Design: Slider 퍼블리싱

---
+++ src/_component/Slider/Slider.tsx
@@ -0,0 +1,49 @@
+interface Props {
+  title: React.ReactNode | string;
+  desc: React.ReactNode | string;
+  img: string;
+  bg: string;
+}
+
+import styles from "./Slider.module.css";
+
+export default function Slider({ title, desc, img, bg }: Props) {
+  return (
+    <div className={styles.wrap}>
+      <div className={`wrap`}>
+        <div className={`flex center between`}>
+          <div>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.desc}>{desc}</p>
+            <button
+              style={{
+                backgroundColor: bg,
+              }}
+              className={styles.login_btn}
+            >
+              로그인
+            </button>
+          </div>
+
+          <img src={img} alt="슬라이더 배너" className={styles.banner_img} />
+        </div>
+      </div>
+      <div className={`${styles.slider_dot} flex between`}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div
+        style={{
+          backgroundColor: bg,
+          width: "40%",
+          height: "100%",
+          top: 0,
+          right: 0,
+          position: "absolute",
+          zIndex: -1,
+        }}
+      ></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import Slider from "../_component/Slider/Slider";
+
 export default function RootPage() {
-  return <>init</>;
+  return (
+    <>
+      <Slider
+        title={
+          <>
+            자신의 멋진 그림을 <br />
+            공유해보아요
+          </>
+        }
+        desc={
+          <>
+            자신의 그림을 함께 나누며
+            <br />
+            소통해보아요
+          </>
+        }
+        img="/slider_img01.jpg"
+        bg="#0E356C"
+      />
+    </>
+  );
 }
