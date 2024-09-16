a010c0c93cc4328c23a3f1abaaddd5d4ed1e0792
chaechae66 <valueworkerchae@gmail.com>
2024-09-16 10:31:26 +0900

    Chore: footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import Header from "../_component/Header/Header";
 
 import "../index.css";
+import "../_style/Common.css";
+import styles from "./layout.module.css";
 
 export const metadata: Metadata = {
   title: "Drawing",
@@ -18,7 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        <div id="root">{children}</div>
+        <div id="root" className={styles.content}>
+          {children}
+        </div>
+        <footer className={`flex center ${styles.footer}`}>
+          copyright&copy; 2024 All rights reserved
+        </footer>
       </body>
     </html>
   );
