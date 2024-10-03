5f86343dfa4eefd1b325b8b6b7f120e435620618
chaechae66 <valueworkerchae@gmail.com>
2024-10-03 20:19:30 +0900

    Refactor: Best 세션 백엔드 데이터 형태에 맞춰 변경

--- src/_component/Best/Best.tsx
+++ src/_component/Best/Best.tsx
@@ -6,57 +6,80 @@ import styles from "./Best.module.scss";
 
 import { PanInfo, motion, useAnimation } from "framer-motion";
 
+const dummy = {
+  likes: [
+    {
+      seq: 0,
+      title: "제목",
+      file: "/example_01.jpg",
+      reg_dt: "2024-10-03",
+      view_cnt: 1242,
+      like_cnt: 1242,
+      comment_cnt: 32,
+    },
+    {
+      seq: 1,
+      title: "제목",
+      file: "/example_02.jpg",
+      reg_dt: "2024-10-03",
+      view_cnt: 1242,
+      like_cnt: 1242,
+      comment_cnt: 32,
+    },
+    {
+      seq: 2,
+      title: "제목",
+      file: "/example_03.jpg",
+      reg_dt: "2024-10-03",
+      view_cnt: 1242,
+      like_cnt: 1242,
+      comment_cnt: 32,
+    },
+  ],
+  views: [
+    {
+      seq: 3,
+      title: "제목",
+      file: "/example_04.jpg",
+      reg_dt: "2024-10-03",
+      view_cnt: 1242,
+      like_cnt: 1242,
+      comment_cnt: 32,
+    },
+    {
+      seq: 4,
+      title: "제목",
+      file: "/example_05.jpg",
+      reg_dt: "2024-10-03",
+      view_cnt: 1242,
+      like_cnt: 1242,
+      comment_cnt: 32,
+    },
+    {
+      seq: 5,
+      title: "제목",
+      file: "/example_06.jpg",
+      reg_dt: "2024-10-03",
+      view_cnt: 1242,
+      like_cnt: 1242,
+      comment_cnt: 32,
+    },
+  ],
+};
+
+const likes = dummy.likes.map((item, index) => ({
+  ...item,
+  rank: `좋아요수 ${index + 1}등`,
+}));
+const views = dummy.views.map((item, index) => ({
+  ...item,
+  rank: `조회수 ${index + 1}등`,
+}));
+
+const data = likes.concat(views);
+
 export default function Best() {
-  const [slides, setSlides] = useState([
-    {
-      id: 1,
-      src: "/example_01.jpg",
-      label: "좋아요수 2위",
-      rank: "닉네임1",
-      views: 1578,
-      likes: 1578,
-    },
-    {
-      id: 2,
-      src: "/example_02.jpg",
-      label: "좋아요수 3위",
-      rank: "닉네임2",
-      views: 1578,
-      likes: 1578,
-    },
-    {
-      id: 3,
-      src: "/example_03.jpg",
-      label: "조회수 1위",
-      rank: "닉네임3",
-      views: 1578,
-      likes: 1578,
-    },
-    {
-      id: 4,
-      src: "/example_04.jpg",
-      label: "조회수 2위",
-      rank: "닉네임4",
-      views: 1578,
-      likes: 1578,
-    },
-    {
-      id: 5,
-      src: "/example_05.jpg",
-      label: "조회수 3위",
-      rank: "닉네임4",
-      views: 1578,
-      likes: 1578,
-    },
-    {
-      id: 6,
-      src: "/example_06.jpg",
-      label: "좋아요수 1위",
-      rank: "닉네임4",
-      views: 1578,
-      likes: 1578,
-    },
-  ]);
+  const [slides, setSlides] = useState(data);
 
   const [currentSlide, setCurrentSlide] = useState(
     Math.floor(slides.length / 2)
@@ -101,48 +124,63 @@ export default function Best() {
             onDragEnd={handleDragEnd}
             animate={controls}
           >
-            {slides.map(({ id, label, rank, views, likes, src }, index) => (
-              <motion.div
-                className={styles.slider_item}
-                key={id}
-                animate={{
-                  scale: scales[index],
-                }}
-              >
-                <div className={styles.wrapper}>
-                  <div className={`${styles.bg} flex column between`}>
-                    <h4 className={styles.item_title}>{label}</h4>
-                    <div className="flex between dark-nobg">
-                      <span>{rank}님</span>
-                      <div className="flex dark-nobg">
-                        <div className={`${styles.info} dark-nobg`}>
-                          <img
-                            className={styles.icon}
-                            src="/view.svg"
-                            alt="조회수_아이콘"
-                          />
-                          <span>{views}</span>
-                        </div>
-                        <div className={`${styles.info} dark-nobg`}>
-                          <img
-                            className={styles.icon}
-                            src="/like.svg"
-                            alt="좋아요수_아이콘"
-                          />
-                          <span>{likes}</span>
+            {slides.map(
+              (
+                { seq, title, file, view_cnt, like_cnt, rank, comment_cnt },
+                index
+              ) => (
+                <motion.div
+                  className={styles.slider_item}
+                  key={seq}
+                  animate={{
+                    scale: scales[index],
+                  }}
+                >
+                  <div className={styles.wrapper}>
+                    <div className={`${styles.bg} flex column between`}>
+                      <h4 className={styles.item_title}>
+                        {rank}
+                        <div className="dark-nobg">{title}</div>
+                      </h4>
+                      <div className="flex between dark-nobg">
+                        <div className="flex dark-nobg">
+                          <div className={`${styles.info} dark-nobg`}>
+                            <img
+                              className={styles.icon}
+                              src="/view.svg"
+                              alt="조회수_아이콘"
+                            />
+                            <span>{view_cnt}</span>
+                          </div>
+                          <div className={`${styles.info} dark-nobg`}>
+                            <img
+                              className={styles.icon}
+                              src="/like.svg"
+                              alt="좋아요수_아이콘"
+                            />
+                            <span>{like_cnt}</span>
+                          </div>
+                          <div className={`${styles.info} dark-nobg`}>
+                            <img
+                              className={styles.icon}
+                              src="/comment.svg"
+                              alt="댓글수_아이콘"
+                            />
+                            <span>{comment_cnt}</span>
+                          </div>
                         </div>
                       </div>
                     </div>
+                    <div
+                      className={styles.image}
+                      style={{
+                        backgroundImage: `url(${file})`,
+                      }}
+                    ></div>
                   </div>
-                  <div
-                    className={styles.image}
-                    style={{
-                      backgroundImage: `url(${src})`,
-                    }}
-                  ></div>
-                </div>
-              </motion.div>
-            ))}
+                </motion.div>
+              )
+            )}
           </motion.div>
         </div>
       </div>
