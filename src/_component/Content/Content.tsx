725894eb677f1808c2866a1893dd6a3a38ca88d2
chaechae66 <valueworkerchae@gmail.com>
2024-10-05 13:52:31 +0900

    Refactor: 데이터 타입에 맞게 더미데이터 수정

--- src/_component/Content/Content.tsx
+++ src/_component/Content/Content.tsx
@@ -7,48 +7,94 @@ import { Variants, motion } from "framer-motion";
 // 더미데이터
 const dummy = [
   {
-    id: 0,
-    img: "/example_01.jpg",
+    seq: 0,
+    title: "제목",
+    file: "/example_01.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 1,
-    img: "/example_02.jpg",
+    seq: 1,
+    title: "제목",
+    file: "/example_01.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 2,
-    img: "/example_03.jpg",
+    seq: 2,
+    title: "제목",
+    file: "/example_05.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 3,
-    img: "/example_04.jpg",
+    seq: 3,
+    title: "제목",
+    file: "/example_06.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 4,
-    img: "/example_05.jpg",
+    seq: 4,
+    title: "제목",
+    file: "/example_01.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 5,
-    img: "/example_06.jpg",
+    seq: 5,
+    title: "제목",
+    file: "/example_02.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 6,
-    img: "/example_05.jpg",
+    seq: 6,
+    title: "제목",
+    file: "/example_03.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 7,
-    img: "/example_04.jpg",
+    seq: 7,
+    title: "제목",
+    file: "/example_05.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 8,
-    img: "/example_03.jpg",
+    seq: 8,
+    title: "제목",
+    file: "/example_01.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
   {
-    id: 9,
-    img: "/example_02.jpg",
-  },
-  {
-    id: 10,
-    img: "/example_01.jpg",
+    seq: 9,
+    title: "제목",
+    file: "/example_05.jpg",
+    reg_dt: "2024-10-03",
+    view_cnt: 1242,
+    like_cnt: 1242,
+    comment_cnt: 32,
   },
 ];
 
@@ -68,23 +114,58 @@ const cardVariants: Variants = {
 };
 export default function Content() {
   return (
-    <section className="wrap">
+    <section className={`wrap ${styles.section}`}>
       <div>
         <h3 className={styles.title}>그림 리스트</h3>
         <p className={styles.desc}>사람들이 공유한 그림들을 구경해보아요</p>
         <div className={styles.image_box}>
-          {dummy.map(({ id, img }) => (
-            <motion.div
-              key={id}
-              initial="offscreen"
-              whileInView="onscreen"
-              variants={cardVariants}
-              transition={{ duration: 1 }}
-              viewport={{ once: true, amount: 0.8 }}
-            >
-              <img src={img} alt={img} className={styles.image} />
-            </motion.div>
-          ))}
+          {dummy.map(
+            ({ seq, file, title, view_cnt, like_cnt, comment_cnt }) => (
+              <motion.div
+                key={seq}
+                initial="offscreen"
+                whileInView="onscreen"
+                variants={cardVariants}
+                transition={{ duration: 1 }}
+                viewport={{ once: true, amount: 0.8 }}
+              >
+                <div className={styles.wrapper}>
+                  <div className={`${styles.bg} flex column between`}>
+                    <h4 className={styles.item_title}>{title}</h4>
+                    <div className="flex between dark-nobg">
+                      <div className="flex dark-nobg">
+                        <div className={`${styles.info} dark-nobg`}>
+                          <img
+                            className={styles.icon}
+                            src="/view.svg"
+                            alt="조회수_아이콘"
+                          />
+                          <span>{view_cnt}</span>
+                        </div>
+                        <div className={`${styles.info} dark-nobg`}>
+                          <img
+                            className={styles.icon}
+                            src="/like.svg"
+                            alt="좋아요수_아이콘"
+                          />
+                          <span>{like_cnt}</span>
+                        </div>
+                        <div className={`${styles.info} dark-nobg`}>
+                          <img
+                            className={styles.icon}
+                            src="/comment.svg"
+                            alt="댓글수_아이콘"
+                          />
+                          <span>{comment_cnt}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <img src={file} alt={title} className={styles.image} />
+                </div>
+              </motion.div>
+            )
+          )}
         </div>
       </div>
     </section>
