7351b8430a9c9d8a400f4fc57dd10b5a1350141b
chaechae66 <valueworkerchae@gmail.com>
2024-11-18 08:29:58 +0900

    Chore: next config에서 next 동적 라우팅 안되는 거 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "./dist", // Changes the build output directory to `./dist/`.
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "http://16.171.252.182:8080/api/:path*",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
